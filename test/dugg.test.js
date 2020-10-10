2a0d9062adb06d1f3baec1266f792b50d08aa21b
Vidar Eldøy <vidar@eldoy.com>
2020-10-10 23:37:00 +0200

    Revert test

--- test/dugg.test.js
+++ test/dugg.test.js
@@ -27,7 +27,7 @@ const file = {
   lastModifiedDate: new Date('2019-07-31T08:00:19.944Z')
 }
 
-describe('dugg', () => {
+describe('convert', () => {
   beforeAll(() => {
     // Delete processed files
     const paths = [p(n.original), p(n.retina)]
@@ -60,8 +60,9 @@ describe('dugg', () => {
     const tmp = `${tmpdir()}/${n.url}`
     const path = await dugg.download(`http://f.o4.no/${n.url}`, tmp)
     expect(path).toBe(tmp)
-    const stats = fs.statSync(path)
-    expect(stats.size).toEqual(80568)
+    const info = dugg.info(path)
+    expect(info.file_name).toBe(n.url)
+    expect(info.file_size).toBe('79 kB')
   })
 
   it('should upload a file', async () => {
